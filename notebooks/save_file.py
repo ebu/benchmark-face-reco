2c20851bcc9db0d8ff5117b03b285c1fbbd4d563
alexandre-rouxel <alexandre.rouxel@gmail.com>
2022-02-09 13:18:58 +0100

    helper functions for files management

---
+++ notebooks/save_file.py
@@ -0,0 +1,7 @@
+def save_data(file_out_np: str, data_np:  np.array,
+              file_out_json: str,  metadata: dict) -> None:
+
+    with open(file_out_json + '.json', 'w') as fp:
+        json.dump(metadata, fp)
+
+    np.save(file=file_out, arr=data_np)
