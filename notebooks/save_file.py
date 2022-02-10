95c4c0ac40538ca315d610ec7910be862dc73117
alexandre-rouxel <alexandre.rouxel@gmail.com>
2022-02-10 09:55:44 +0100

    typo in file names

--- notebooks/save_file.py
+++ notebooks/save_file.py
@@ -1,7 +1,10 @@
+import numpy as np
+import json
+
 def save_data(file_out_np: str, data_np:  np.array,
               file_out_json: str,  metadata: dict) -> None:
 
     with open(file_out_json + '.json', 'w') as fp:
         json.dump(metadata, fp)
 
-    np.save(file=file_out, arr=data_np)
+    np.save(file=file_out_np, arr=data_np)
